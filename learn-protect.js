5b5ef2bbdae2e78b915364c6764b0efc61c1c98d
ibCom <hello@ibcom.biz>
2022-05-07 11:07:38 +1000

    protect

--- learn-protect.js
+++ learn-protect.js
@@ -92,9 +92,9 @@ exports.handler = function (event, context, callback)
 
                 var event = entityos.get({ scope: '_event'});
 
-                if (event.hash == undefined)
+                if (event.hashMethod == undefined)
                 {
-                    event.hash = 'sha256'
+                    event.hashMethod = 'sha256'
                 }
 
                 if (event.output == undefined)
@@ -102,15 +102,217 @@ exports.handler = function (event, context, callback)
                     event.output = 'base64'
                 }
 
-                var textHashed = createHash(event.hash).update(event.text).digest(event.output);
+                event.textHashed = createHash(event.hashMethod).update(event.text).digest(event.output);
 
-                console.log('util-protect-hash:' + event.hash + ':' + event.output + ':');
-                entityos.invoke('util-end',
+                entityos.invoke('util-end', event);     
+            }
+        });
+
+        entityos.add(
+        {
+            name: 'util-protect-hash-with-key',
+            code: function ()
+            {
+                const { createHmac } = require('crypto');
+
+                var event = entityos.get({ scope: '_event'});
+
+                if (event.hashMethod == undefined)
                 {
-                    context: 'util-protect-hash:' + event.hash + ':' + event.output,
-                    text: event.text,
-                    textHashed: textHashed
-                });     
+                    event.hashMethod = 'sha256'
+                }
+
+                if (event.output == undefined)
+                {
+                    event.output = 'base64'
+                }
+
+                event.textHashedWithKey = createHmac(event.hashMethod, event.keyPrivate).update(event.text).digest(event.output);
+
+                entityos.invoke('util-end', event);     
+            }
+        });
+
+        entityos.add(
+        {
+            name: 'util-protect-hash-with-salt',
+            code: function ()
+            {
+                //LEARN; Let's add some salt to mix it up a bit
+
+                const { scryptSync, randomBytes, timingSafeEqual } = require('crypto');
+
+                var event = entityos.get({ scope: '_event'});
+
+                if (event.output == undefined)
+                {
+                    event.output = 'hex' // 'base64'
+                }
+
+                event.salt = randomBytes(16).toString('hex');
+                event.textHashedWithSalt = scryptSync(event.text,  event.salt, 64).toString(event.output);
+
+                entityos.invoke('util-end', event);     
+            }
+        });
+
+        entityos.add(
+        {
+            name: 'util-protect-create-keys',
+            code: function ()
+            {
+                const { generateKeyPairSync } = require('crypto');
+
+                var event = entityos.get({ scope: '_event'});
+
+                if (event.keyMethod == undefined)
+                {
+                    event.keyMethod = 'rsa'
+                }
+
+                if (event.keyLength == undefined)
+                {
+                    event.keyLength = 2048
+                }
+
+                if (event.output == undefined)
+                {
+                    event.output = 'base64'
+                }
+
+                event.keyPublicType = 'spki' // recommended to be 'spki' by the Node.js docs
+                event.keyPrivateType = 'pkcs8' // recommended to be 'spki' by the Node.js docs
+
+                const { privateKey, publicKey } = generateKeyPairSync(event.keyMethod,
+                {
+                    modulusLength: event.keyLength, // the length of your key in bits
+                    publicKeyEncoding:
+                    {
+                        type: event.keyPublicType,
+                        format: 'pem',
+                    },
+                    privateKeyEncoding:
+                    {
+                        type: event.keyPrivateType,
+                        format: 'pem',
+                        cipher: event.keyCipher // 'aes-256-cbc',
+                        // passphrase: event.keyCipherSecret // 'top secret'
+                    },
+                });
+                  
+                event.keyPrivate = privateKey;
+                event.keyPublic = publicKey;
+
+                entityos.invoke('util-end', event);     
+            }
+        });
+
+        entityos.add(
+        {
+            name: 'util-protect-encrypt',
+            code: function ()
+            {
+                const { createCipheriv, randomBytes } = require('crypto');
+
+                var event = entityos.get({ scope: '_event'});
+
+                if (event.keyPrivate == undefined)
+                {
+                    event.keyPrivate = randomBytes(32);
+                }
+
+                if (event.initialisationVector == undefined)
+                {
+                    event.initialisationVector = randomBytes(16);
+                }
+
+                event.iv =  event.initialisationVector.toString('hex');
+                event.key =  event.keyPrivate.toString('hex');
+
+                if (event.encryptionMethod == undefined)
+                {
+                    event.encryptionMethod = 'aes256'
+                }
+
+                const cipher = createCipheriv(event.encryptionMethod, event.keyPrivate, event.initialisationVector);
+
+                if (event.output == undefined)
+                {
+                    event.output = 'hex' // 'base64'
+                }
+
+                event.textEncrypted = cipher.update(event.text, 'utf8', event.output) + cipher.final(event.output);
+
+                entityos.invoke('util-end', event);     
+            }
+        });
+    
+        entityos.add(
+        {
+            name: 'util-protect-decrypt',
+            code: function ()
+            {
+                const { createDecipheriv, randomBytes } = require('crypto');
+
+                var event = entityos.get({ scope: '_event'});
+
+                event._keyPrivate = new Buffer.from(event.keyPrivate, 'hex');
+                event._initialisationVector = new Buffer.from(event.initialisationVector, 'hex');
+                
+                if (event.encryptionMethod == undefined)
+                {
+                    event.encryptionMethod = 'aes256'
+                }
+
+                const decipher = createDecipheriv(event.encryptionMethod, event._keyPrivate, event._initialisationVector);
+
+                if (event.output == undefined)
+                {
+                    event.output = 'hex' // 'base64'
+                }
+
+                event.textDecrypted = decipher.update(event.text, event.output, 'utf8') + decipher.final('utf8');
+
+                entityos.invoke('util-end', event);     
+            }
+        });
+
+        entityos.add(
+        {
+            name: 'util-protect-sign',
+            code: function ()
+            {
+                const { createSign, createVerify } = require('crypto');
+                const { publicKey, privateKey } = require('./keypair');
+
+                var event = entityos.get({ scope: '_event'});
+
+                /*const signer = createSign('rsa-sha256');
+
+                signer.update(message);
+
+                const signature = signer.sign(privateKey, 'hex');
+
+
+                event._keyPrivate = new Buffer.from(event.keyPrivate, 'hex');
+                event._initialisationVector = new Buffer.from(event.initialisationVector, 'hex');
+                
+                if (event.encryptionMethod == undefined)
+                {
+                    event.encryptionMethod = 'aes256'
+                }
+
+                const decipher = createDecipheriv(event.encryptionMethod, event._keyPrivate, event._initialisationVector);
+
+                if (event.output == undefined)
+                {
+                    event.output = 'hex' // 'base64'
+                }
+
+                event.textDecrypted = decipher.update(event.text, event.output, 'utf8') + decipher.final('utf8');
+                */
+
+                entityos.invoke('util-end', event);     
             }
         });
 
